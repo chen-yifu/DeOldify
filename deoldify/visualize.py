0a87767343dc5449850f8d64b4d6ce610499c3ba
Yifu Chen <45907512+chen-yifu@users.noreply.github.com>
2021-10-16 20:35:10 -0700

    Update visualize.py

--- deoldify/visualize.py
+++ deoldify/visualize.py
@@ -338,6 +338,7 @@ class VideoColorizer:
         source_path = self.source_folder / file_name
 #        self._download_video_from_url(source_url, source_path)
         source_path = input("-"*100+"\n请您从左侧copy电影路径，并按回车键\n")
+        source_path = Path(source_path)
         return self._colorize_from_path(
             source_path, render_factor=render_factor, post_process=post_process,watermarked=watermarked
         )
